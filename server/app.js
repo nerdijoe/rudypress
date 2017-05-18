68b4321e748f0303f4aac9724e4f4b12b55eb5b0
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-05-18 13:13:02 +0700

    connect to mongoose

--- server/app.js
+++ server/app.js
@@ -3,7 +3,15 @@ var bodyParser = require('body-parser')
 
 var index = require('./routes/index')
 
-var app = express()
+var app = express();
+
+var mongoose = require('mongoose');
+var mongoDbConfig = 'mongodb://localhost/rudypress'
+
+mongoose.connect(mongoDbConfig, (err, res) => {
+  console.log('connected to DB: ' + mongoDbConfig);
+})
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
