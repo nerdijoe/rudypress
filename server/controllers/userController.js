8ce298459f7445f2f4e41a50a7a33e05eef82ed6
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-05-18 13:24:14 +0700

    create user, and password-hash his password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,13 @@
 var User = require('../models/user')
-
+var passwordHash = require('password-hash')
 
 exports.create = (req, res, next) => {
+  req.body.password = passwordHash.generate(req.body.password)
+
   User.create(req.body, (err, user) => {
     if(err) res.send(err)
     else {
+      console.log('User create', user)
       res.send(user)
     }
   })
